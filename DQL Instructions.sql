68394839ae7fee132bb6c80ac4fc5428afd139be
alexLazar30 <82840334+alexLazar30@users.noreply.github.com>
2024-09-09 10:41:24 +0300

    Update DQL Instructions.sql

--- DQL Instructions.sql
+++ DQL Instructions.sql
@@ -1,34 +1,34 @@
 # 1. On the line below we wil select all the data from the 'children' table
  select * from children;
  
-# 2. On the line below we will select columns: firstname, last_name and age from 'children' table
+# 2. On the line below we will select firstname, last_name and age from 'children' table
 select firstname, last_name, age from children;
 
 # 3. On the line below we will select all the children with age of 10.
 select * from children where age=10; 
 
-# 4. On the line below we will select all the children with total absences lower than 20 from the table 'child_situation'
+# 4. On the line below we will select all the children with total absences lower than 20
 select current_child_id, total_absences from child_situation where total_absences<=10;
 
 # 5. On the line below we will select all the children with the total abscences higher than 30
 select id, current_child_id, total_absences from child_situation where total_absences >=30;
 
-# 6. On the line below we will select all the children with lastname Mihai from the table 'children'
+# 6. On the line below we will select all the children with lastname Mihai
 select * from children where last_name='Mihai';
 
-# 7. On the line below we will select all the children whose year of birth is '2015' from  the table 'children'
+# 7. On the line below we will select all the children whose year of birth is '2015'
 select * from children where date_of_birth like '2015%';
 
-# 8. On the line below we will select all the girls name from the class whose year of birth is '2012' from the table 'children'
+# 8. On the line below we will select all the girls name from the class whose year of birth is '2012'
 select last_name, date_of_birth from children where date_of_birth like '2012%' and gender='F';
 
 # 9. On the line below we will select all the full names based on the first name that starts with 'I'
 select firstname, last_name from children where firstname like'I%';
 
-# 10. On the line below we will select all the children last_name with age of 10 and 13 from the table 'children'
+# 10. On the line below we will select all the children last_name with age of 10 and 13
 select last_name, age from children where age=10 or age=13;
 
-# 11. On the line below we will select all the child id's based on total absences with the value of 30 and the closest value higher than 30
+# 11. On the line below we will select children who have a number of absences of 30 and 35
 select current_child_id from child_situation where total_absences=30 or total_absences=35;
 
 # 12. On the line below we will select all the children who are not 10 years old and are boys
@@ -37,27 +37,25 @@ select  firstname, last_name, age from children where not age=10 and gender='M';
 # 13. On the line below we will select all the math grades except of grade 5 and 10
 select id, child_id, math from classbook where not math=5 and not math=10; 
 
-# 14. On the line below the average value for the total absences will be calculated from the 'child_situation' table 
+# 14. On the line below we will calculate the average value for the total absences
 select AVG(total_absences) as average_absences from child_situation;
  
 # 15. On the line below we will calculate the number of the children with the age of 13 years old
 select COUNT(age) as children_with_thirteen_years_old from children where age=13;
 
-# 16. On the line below we will calculate the number of girls from the school from the 'children' table
+# 16. On the line below we will calculate the number of girls from the school
 select count(gender) as no_of_girls from children where gender='F';
 
-# 17. On the line below we will calculate the total enrolled from the mandatory disciplines from the 'mandatory_disciplines' table
+# 17. On the line below we will calculate the total enrolled from the mandatory disciplines
 select SUM(enrolled) as total_enrolled from mandatory_disciplines;
 
-# 18. On the line below we will calculate the total enrolled from the optional disciplines from the 'optional_disciplines' table
+# 18. On the line below we will calculate the total enrolled from the optional disciplines
 select SUM(enrolled) as total_enrolled from optional_disciplines;
 
-# 19. On the line below we will calculate the child with the higher number of absences from the 'child_situation' table by using an aggregate function in a subquery 
-
-select * from child_situation
- where total_absences=( select MAX(total_absences) from child_situation );
+# 19. On the line below we will calculate the child with the higher number of absences, using an aggregate function in a subquery
+ select * from child_situation where total_absences=( select MAX(total_absences) from child_situation );
   
- # 20. On the line below we will calculate the school curriculum with the lowest number of enrolled students from the 'optional_disciplines' table by using an aggregate function in a subquery 
+ # 20. On the line below, we will calculate the school curriculum with the lowest number of enrolled students by using an aggregate function in a subquery
 select * from optional_disciplines
 where enrolled=( select MIN(enrolled) from optional_disciplines);
 
@@ -65,8 +63,7 @@ where enrolled=( select MIN(enrolled) from optional_disciplines);
 select child_id, firstname, last_name, age, date_of_birth, gender, math, physics, english, art, neuroscience_principles, physical_education
 from children cross join classbook where children.id=classbook.child_id order by children.id;
  
-# 22. On the line below we will join tables 'children' and 'child_situation' in order to get the fullname of the child and the
-# total abscences using Inner Join statement
+# 22. On the line below we will get the fullname of the child and the total abscences using Inner Join statement between 'children' and 'child_situation' tables
 
 select current_child_id, firstname, last_name, number_of_allowed_absences, total_absences from children
 inner join child_situation on children.id=child_situation.current_child_id;
